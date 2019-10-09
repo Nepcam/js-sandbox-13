044bd2f3d38a81e1c1339ec25e246a7e1306f347
Nepcam <cameron.nepe@gmail.com>
2019-10-09 21:35:54 +1300

    error handling

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 try {
   // Produces a ReferenceError
-  myFunction();
+  // myFunction();
+
+  // Produce a TypeError
+  null.myFunction();
+
 } catch(e) {
-  // console.log(e);
-  console.log(e.message);
+  console.log(`${e.name}: ITS NULL STUPID`);
+  console.log(e);
+  // console.log(e.message); 
+  // console.log(e.name); // Gives us the type of error ReferenceError
+  // console.log(e instanceof ReferenceError);
+  // console.log(e instanceof TypeError);
+} finally {
+  console.log('Finally runs regardless of result...')
 }
 
 console.log('Program continues...');
